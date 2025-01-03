4e34275d0a2e6b7876df2405fa285c7a5135591d
mastan0713D <mastan99zphs@gmail.com>
2025-01-03 19:32:47 +0530

    Day 3 Siri Wave

---
+++ engine/features.py
@@ -0,0 +1,6 @@
+from playsound import playsound
+
+##Playsing Assisant sound function
+def playAssistantSound():
+    music_dir= "D:\\MY PROJECTS\\JARVIS\\www\\assets\\audio\\start_sound.mp3"
+    playsound(music_dir)
